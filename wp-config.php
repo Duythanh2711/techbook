8118af1ef9093b27e558c147882dc48a0cf4a030
mrttahtecom <anh.trinh@htecom.vn>
2024-10-21 09:40:58 +0700

    ignore logs

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'techbook' );
 define( 'DB_PASSWORD', '123456' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mysql' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
