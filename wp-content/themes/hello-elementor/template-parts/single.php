ef860d9196a3760225dbf7681cb347472e7fe728
Duythanh2711 <duythanh271102@gmail.com>
2024-11-07 20:58:38 +0700

    them wishlish

--- wp-content/themes/hello-elementor/template-parts/single.php
+++ wp-content/themes/hello-elementor/template-parts/single.php
@@ -26,7 +26,7 @@ while ( have_posts() ) :
 			include 'techbook/detail_publisher/index.php';
 		} else if(get_the_ID()== 75){
 			include 'techbook/Home/Home.php';
-		} else if(get_the_ID()== 638){
+		} else if(get_the_ID()== 656){
 			include 'techbook/wishlist/index.php';
 			the_content();
 		} else if(get_the_ID()== 427){
