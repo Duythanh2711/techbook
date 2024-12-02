ef1d019f0e3be26773bf9c48d091221d36e72bb5
Hunghtecom <hung.phan@htecom.vn>
2024-12-02 15:31:35 +0700

    Fix number wishlist load ajax

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'techbook' );
+define( 'DB_NAME', 'wp_techbook' );
 
 /** Database username */
-define( 'DB_USER', 'techbook' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '123456' );
+define( 'DB_PASSWORD', 'mysql' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
--- wp-content/themes/hello-elementor/template-parts/techbook/wishlist/index.js
+++ wp-content/themes/hello-elementor/template-parts/techbook/wishlist/index.js
@@ -142,23 +142,26 @@ $(document).ready(function() {
                                     </a>
                                 `;
                             });
-                        }
+                        }   
 
                         output += '</div>';
                         $('.product-list-wishlist').html(output);
                         $('#loading-container').hide();
                     } else {
-                        $('.product-list-wishlist').html('<p>An error occurred while loading data.</p>');
-                        $('#loading-container').hide();
+                        $('.product-list-wishlist').html('<p>There are no products in the Wishlist.</p>');
+                        $('#wishlist-count').text(`(0)`);
+                        $('#loading-container').hide(); 
                     }
                 },
                 error: function(xhr, status, error) {
-                    alert('An error occurred while loading data. Please try again later.');
+                    alert('There are no products in the Wishlist. Please try again later.');
+                    $('#wishlist-count').text(`(0)`);
                     $('#loading-container').hide();
                 }
             });
         } else {
-            $('.product-list-wishlist').html('<p>No product IDs are stored.</p>');
+            $('.product-list-wishlist').html('<p>There are no products in the Wishlist.</p>');
+            $('#wishlist-count').text(`(0)`);
             $('#loading-container').hide();
         }
     }
